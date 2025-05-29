d44852fa454051c26aed42d34fd1555ede15bb5a
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-05-29 15:08:48 +0000

    fix: screenrc escape characters

--- cloudshell.tf
+++ cloudshell.tf
@@ -184,6 +184,12 @@ resource "azurerm_linux_virtual_machine" "cloudshell_vm" {
   custom_data = base64encode(
     templatefile("${path.module}/cloud-init/CLOUDSHELL.conf",
       {
+        VAR_ssh_host_rsa_private     = tls_private_key.cloudshell_host_rsa.private_key_pem
+        VAR_ssh_host_rsa_public      = tls_private_key.cloudshell_host_rsa.public_key_openssh
+        VAR_ssh_host_ecdsa_private   = tls_private_key.cloudshell_host_ecdsa.private_key_pem
+        VAR_ssh_host_ecdsa_public    = tls_private_key.cloudshell_host_ecdsa.public_key_openssh
+        VAR_ssh_host_ed25519_private = tls_private_key.cloudshell_host_ed25519.private_key_pem
+        VAR_ssh_host_ed25519_public  = tls_private_key.cloudshell_host_ed25519.public_key_openssh
         VAR_Directory_tenant_ID      = var.cloudshell_Directory_tenant_ID
         VAR_Directory_client_ID      = var.cloudshell_Directory_client_ID
         VAR_Forticnapp_account       = var.Forticnapp_account
