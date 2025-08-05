7e9469677473cf83d3395d5955e2b730216efd50
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-08-04 23:57:50 -0400

    Fix circular reference in cloudshell public IP data source
    - Remove unnecessary data.azurerm_public_ip.cloudshell_public_ip data source
    - Directly reference azurerm_public_ip.cloudshell_public_ip[count.index].fqdn
    - Eliminates circular dependency where data source referenced the same resource
    - Terraform validation passes successfully

--- data.tf
+++ data.tf
@@ -10,9 +10,3 @@ data "azurerm_public_ip" "hub_nva_management_public_ip" {
   name                = azurerm_public_ip.hub_nva_management_public_ip[0].name
   resource_group_name = azurerm_resource_group.azure_resource_group.name
 }
-
-data "azurerm_public_ip" "cloudshell_public_ip" {
-  count               = var.cloudshell ? 1 : 0
-  name                = azurerm_public_ip.cloudshell_public_ip[count.index].name
-  resource_group_name = azurerm_resource_group.azure_resource_group.name
-}
