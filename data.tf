a5aad840541791bb2436111e0177d34b0a4e007e
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-08-04 23:47:01 -0400

    fix(cloudshell): resolve index out of range error for conditional resources
    - Replace hardcoded [0] indexes with count.index in cloudshell resources
    - Fix azurerm_dns_cname_record.cloudshell_public_ip_dns to use count.index
    - Fix data.azurerm_public_ip.cloudshell_public_ip to use count.index
    - Ensures proper conditional resource creation when var.cloudshell = false
    
    Resolves terraform plan error: 'index [0] is out of range for count'

--- data.tf
+++ data.tf
@@ -13,6 +13,6 @@ data "azurerm_public_ip" "hub_nva_management_public_ip" {
 
 data "azurerm_public_ip" "cloudshell_public_ip" {
   count               = var.cloudshell ? 1 : 0
-  name                = azurerm_public_ip.cloudshell_public_ip[0].name
+  name                = azurerm_public_ip.cloudshell_public_ip[count.index].name
   resource_group_name = azurerm_resource_group.azure_resource_group.name
 }
